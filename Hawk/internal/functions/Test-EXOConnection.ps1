4ca5adc314b73bd9ec1cd30fa5a04fe247c3201c
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-11-25 11:38:45 +0100

    Update Test-EXOConnection.ps1

--- Hawk/internal/functions/Test-EXOConnection.ps1
+++ Hawk/internal/functions/Test-EXOConnection.ps1
@@ -20,15 +20,15 @@ function Test-EXOConnection {
     }
     catch [System.Management.Automation.CommandNotFoundException] {
         # Connect to EXO if we couldn't find the command
-        Out-LogFile "Not Connected to Exchange Online" -Information
-        Out-LogFile "Connecting to EXO using Exchange Online Module using Browser Based Authentication" -Action
+        Out-LogFile 'Not Connected to Exchange Online' -Information
+        Out-LogFile 'Connecting to EXO using Exchange Online Module using Browser Based Authentication' -Action
 
         # Beginning in Exchange Online PowerShell module version 3.7.0, Microsoft is implementing Web Account Manager (WAM) as the default authentication broker for user authentication. 
-        if ((Get-Module -Name ExchangeOnlineManagement).Version -ge [version]'3.7.0') {
-            Connect-ExchangeOnline -ShowBanner:$false
+        if ((Get-Command -Name Connect-ExchangeOnline).Version -ge [version]'3.7.0') {
+            Connect-ExchangeOnline -ShowBanner:$false -DisableWAM
         }
         else {
-            Connect-ExchangeOnline -ShowBanner:$false -DisableWAM
+            Connect-ExchangeOnline -ShowBanner:$false
         }
     }
 }
